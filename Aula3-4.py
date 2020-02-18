dd4493c23387e992b5ddc4fd7d51d0f53db34f1c
pbmarques <pgmarques23@gmail.com>
2020-02-18 16:32:39 -0300

    adicionar exercicios

---
+++ Aula3-4.py
@@ -0,0 +1,16 @@
+tabela = []
+linha = 0
+coluna = 1
+n = int(input("Coloque o numero de linhas: "))
+caractere = "Digite o caractere: "
+
+print(tabela)
+
+ while coluna = n :
+    while linha = coluna :
+        tabela.append(caractere)
+        linha = linha + 1
+    coluna = coluna + 1
+
+print(tabela)
+
